5a5e7e8c397ae14d781323795997d9c938decc0a
kubomarina <sa1225no@gmail.com>
2021-04-03 08:11:36 +0000

    edit bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,17 +4,31 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
-  def new
+  def show
+    @book = Book.find(params[:id])
   end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to book_path(book.id)
   end
     
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
